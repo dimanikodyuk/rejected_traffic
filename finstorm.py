c5dfe6c183aacb3697385f91fb4e53cdecb7146d
nykodiuk.d <dimanikodyuk@gmail.com>
2021-12-07 08:44:18 +0200

    исправлена ошибка с обновлением токена

--- finstorm.py
+++ finstorm.py
@@ -1,6 +1,6 @@
 from db.models import get_source, get_partner_token, get_lead_list, check_status_admitad
 from resources.api import send_request_admitad, get_status_admitad, send_request_finline, send_request_finstorm
-from resources.api_logs import logger_finline
+from resources.api_logs import logger_finline, logger_finstorm
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -9,8 +9,10 @@ conf = get_source()
 if __name__ == "__main__":
     try:
         for i in conf:
+
             if i["global_name"] == "Finstorm" and i["is_active"] == 1:
                 p_uid = i['uid']
+                logger_finstorm.info("UID: " + str(i['uid']))
                 leads = get_lead_list(i['partner_id'], i['stream_id'])
 
                 for row in leads:
