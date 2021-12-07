bbbab6758d783f47f7f7a2cbb4ba6917d109a422
nykodiuk.d <dimanikodyuk@gmail.com>
2021-12-07 09:34:21 +0200

    оновлення

--- resources/api.py
+++ resources/api.py
@@ -266,8 +266,9 @@ def send_request_finstorm(p_phone, p_inn, p_first_name, p_last_name, p_middle_na
         response = requests.request('POST', url=url_finstorm, data=payload, verify=False)
         #res = response.text
         status = response.status_code
+        print("RESPONSE: " + str(response) + "\n" + str(status))
+
         if status == 200:
-            print("RESPONSE: " + str(response))
             result = json.loads(response.text)
             #print(response)
             logger_finstorm.info("RESPONSE JSON: " + str(response).replace('\'', '`'))
