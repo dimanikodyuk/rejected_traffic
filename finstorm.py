930b6955f11643a5ab3862a0c362c279fe8f0b35
nykodiuk.d <dimanikodyuk@gmail.com>
2021-12-10 09:20:22 +0200

    update finstorm.py file

--- finstorm.py
+++ finstorm.py
@@ -1,6 +1,6 @@
-from db.models import get_source, get_partner_token, get_lead_list, check_status_admitad
-from resources.api import send_request_admitad, get_status_admitad, send_request_finline, send_request_finstorm
-from resources.api_logs import logger_finline, logger_finstorm
+from db.models import get_source, get_lead_list
+from resources.api import send_request_finstorm
+from resources.api_logs import logger_finstorm
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -14,7 +14,6 @@ if __name__ == "__main__":
                 p_uid = i['uid']
                 logger_finstorm.info("UID: " + str(i['uid']))
                 leads = get_lead_list(i['partner_id'], i['stream_id'])
-
                 for row in leads:
                     p_phone = row['client_phone2']
                     p_inn = row['inn']
